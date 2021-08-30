6646bf291af6ae593f261ddf6cec5d6cd0868de7
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-08-30 13:55:19 +0200

    update

---
+++ read_blending_colors.jsx
@@ -0,0 +1,215 @@
+$.level = 0; // Debugging level, Level: 0 - No Break, 1 - Break, 2 - Immediate Break
+
+function doInvisible(items) {
+    for (var i = 0; i < items.length; i++) {
+        items[i].visible = false;
+    }
+}
+
+function doInvisibleLayers() {
+    var doc = app.activeDocument;
+    var notFolderLayers = doc.artLayers;
+
+    doInvisible(notFolderLayers)
+}
+
+function doInvisibleFolders() {
+    var doc = app.activeDocument;
+    var folders = doc.layerSets;
+
+    doInvisible(folders)
+}
+
+function convertValueStringToInt(_unitValue) {
+
+    var string = _unitValue.toString();
+    var nonDecimal = /\D/g;
+    return string.replace(nonDecimal, '');
+}
+
+function leftUpperCornerColorBGSet(layer) {
+
+    var doc = app.activeDocument;
+
+    doc.colorSamplers.removeAll(); // Remove any Color Samplers that may already exist to avoid bug when stack samples is full and it is 4, in that case you can't do any new measurements
+
+    doc.selection.deselect(); // deselct any selection that may already exist.
+
+    var LayerBounds = layer.bounds;
+
+    var x = convertValueStringToInt(LayerBounds[0]);
+    var y = convertValueStringToInt(LayerBounds[1]);
+
+    var pixelLocalisation_X_Y = [x, y];
+
+    var colorSampleObject = null;
+
+    colorSampleObject = doc.colorSamplers.add(pixelLocalisation_X_Y); // "try catch" does not work here
+
+    var sampledColor = colorSampleObject.color.rgb.hexValue;
+    colorSampleObject.remove();
+
+    return sampledColor;
+}
+
+
+function isLayerEmptyCheck(layer) {
+
+    var LayerBounds = layer.bounds;
+
+    if (LayerBounds[0] === "0 px" && LayerBounds[1] === "0 px" && LayerBounds[2] === "0 px" && LayerBounds[3] === "0 px") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getHexColorInLayers(layers) {
+    var RGBHexes = [];
+
+    for (var j = 0; j < layers.length; j++) {
+
+        if (isLayerEmptyCheck(layers[j])) {
+            RGBHexes.push("null");
+            continue;
+        }
+
+        RGBHexes.push(leftUpperCornerColorBGSet(layers[j]));
+        layers[j].visible = false;
+    }
+
+    return RGBHexes;
+}
+
+function doLayersVisible(layers) {
+    for (var j = 0; j < layers.length; j++) {
+        layers[j].visible = true;
+    }
+}
+
+function doBlendingModesNormal(items) {
+    for (var i = 0; i < items.length; i++) {
+        items[i].blendMode = BlendMode.NORMAL;
+    }
+}
+
+function doBlendingModesNormalAllLayersInAllFolders(RGBfolders) {
+    for (var i = 0; i < RGBfolders.length; i++) {
+        var folderRGB = RGBfolders[i];
+        var layers = folderRGB.artLayers;
+        doBlendingModesNormal(layers);
+    }
+}
+
+function prearrangeDocToProcess(COLORSFolder, RGBfolders) {
+
+    doInvisibleLayers();
+    doInvisibleFolders();
+
+    COLORSFolder.visible = true;
+    COLORSFolder.blendMode = BlendMode.NORMAL;
+
+    doBlendingModesNormal(RGBfolders);
+    doBlendingModesNormalAllLayersInAllFolders(RGBfolders);
+}
+
+function getAllHexValues(RGBfolders) {
+    var HexColorValues = [];
+
+    for (var i = 0; i < RGBfolders.length; i++) {
+        var folderRGB = RGBfolders[i];
+        var layers = folderRGB.artLayers;
+        doLayersVisible(layers);
+        HexColorValues.push([folderRGB.name, getHexColorInLayers(layers)]);
+    }
+
+    return HexColorValues;
+}
+
+function writeValuesToCSV(HexColorValues) {
+
+    var text = "Group, R, G, B\n";
+
+    for (var i = 0; i < HexColorValues.length; i++) {
+        var flattenArray = [HexColorValues[i][0]].concat(HexColorValues[i][1]);
+        for (var j = 0; j < flattenArray.length; j++) {
+            text += flattenArray[j]
+            if (j + 1 < flattenArray.length) {
+                text += ", ";
+            } else {
+                text += "\n";
+            }
+        }
+    }
+
+    try {
+        var path = saveTxt(text);
+        return "You successfully saved values to " + path;
+
+    } catch (error) {
+        return error;
+    }
+}
+
+function saveTxt(string) {
+    var doc = app.activeDocument;
+    var name = doc.name.replace(/\.[^\.]+$/, '');
+    var ext = decodeURI(doc.name).replace(/^.*\./, '');
+    if (ext.toLowerCase() != 'psd')
+        return alert("document is not saved!\nSave document on disk first!");
+
+    var docPath = doc.path;
+    var scvPath = docPath + "/" + name + ".csv"
+    var saveFile = File(scvPath);
+
+    if (saveFile.exists)
+        saveFile.remove();
+
+    saveFile.encoding = "UTF8";
+    saveFile.open("e", "TEXT", "????");
+    saveFile.writeln(string);
+    saveFile.close();
+
+    return scvPath;
+}
+
+function main() {
+    try {
+        var doc = app.activeDocument;
+    } catch (error) {
+        return alert("You do not have any opened file!")
+    }
+
+    try {
+        var docPath = doc.fullName;
+    } catch (error) {
+        return alert("To make use of the script, save the document on a drive first.");
+    }
+
+    var saveFile = confirm("Do you want to save file? File will be closed without saving and reopen after executing script.", true, "Warning!");
+    if (saveFile) {
+        doc.save();
+    }
+
+    var COLORSFolder = doc.layerSets.getByName("COLORS");
+
+    if (!COLORSFolder) {
+        return alert('No "COLORS" folder in top hierarchy');
+    }
+    
+    var RGBfolders = COLORSFolder.layerSets;
+
+    prearrangeDocToProcess(COLORSFolder, RGBfolders);
+
+    var HexColorValues = getAllHexValues(RGBfolders);
+
+    var promise = writeValuesToCSV(HexColorValues);
+    alert(promise);
+
+    // reset file
+    doc.close(SaveOptions.DONOTSAVECHANGES);
+    // reload to stage before processing file
+    app.open(File(docPath));
+}
+
+main();

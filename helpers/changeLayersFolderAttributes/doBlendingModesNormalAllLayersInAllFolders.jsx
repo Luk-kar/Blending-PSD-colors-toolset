3657f957193dc99062a0b7f6507fe4b477775c7e
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-09-03 12:44:10 +0200

    update

--- helpers/changeLayersFolderAttributes/doBlendingModesNormalAllLayersInAllFolders.jsx
+++ helpers/changeLayersFolderAttributes/doBlendingModesNormalAllLayersInAllFolders.jsx
@@ -1,19 +1,12 @@
 #include "./doBlendingModesNormal.jsx"
+#include "../gettingLayers/getRGBLayers.jsx";
 
 function doBlendingModesNormalAllLayersInAllFolders(RGBfolders, layersNames) {
     for (var i = 0; i < RGBfolders.length; i++) {
         var folderRGB = RGBfolders[i];
         var layersInFolder = folderRGB.artLayers;
 
-        var RGBLayers = [];
-        for (var j = 0; j < layersNames.length; j++) {
-            try {
-              var foundLayer = layersInFolder.getByName(layersNames[j])
-              RGBLayers.push(foundLayer)
-            }
-            catch(e){
-            } 
-        }
+        var RGBLayers = getRGBLayers(RGBfolders, layersNames);
 
         doBlendingModesNormal(RGBLayers);
     }

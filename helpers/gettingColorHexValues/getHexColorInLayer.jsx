0451f8ff79c95468cc22dee72ecb00e5837d40ea
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-09-03 12:22:53 +0200

    update

--- helpers/gettingColorHexValues/getHexColorInLayer.jsx
+++ helpers/gettingColorHexValues/getHexColorInLayer.jsx
@@ -1,8 +1,26 @@
-#include "../checking/isLayerEmptyCheck.jsx"
-#include "./getLayerLeftUpperCornerColorHex.jsx"
+#include "../checking/isLayerEmptyCheck.jsx";
+#include "./getLayerLeftUpperCornerColorHex.jsx";
+
+function getRGBLayers(layers, layersNames) {
+
+    var RBGLayers = [];
+    for (var i = 0; i < layersNames.length; i++) {
+
+        try {
+            RBGLayers.push(layers.getByName(layersNames[i]));
+        }
+        catch(e){
+            continue;
+        }
+
+    }
+    return RBGLayers;
+}
 
 function getHexColorInLayer(layers, layersNames) {
+
     var RGBHexes = [];
+    var RBGLayers = getRGBLayers(layers, layersNames);
 
     var RBGLayers = [];
     for (var i = 0; i < layersNames.length; i++) {

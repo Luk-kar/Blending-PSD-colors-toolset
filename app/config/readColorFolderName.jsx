599b5c6128a5197f5caece6dabf1756fcebb9de8
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-09-22 16:16:15 +0200

    update

--- app/config/readColorFolderName.jsx
+++ app/config/readColorFolderName.jsx
@@ -1,8 +1,6 @@
 #include "./utils/getConfigValue.jsx"
 #include "./configDefaultValues.jsx"
 
-alert(readColorFolderName());
-
 function readColorFolderName() {
 
     var searchedPhrase = configDefaultValues.fileStructure.color_folder_name.key;
--- app/eventHandlers/createFoldersTemplates/helpers/createCOLORFolderIfNotExists/createCOLORFolderIfNotExists.jsx
+++ app/eventHandlers/createFoldersTemplates/helpers/createCOLORFolderIfNotExists/createCOLORFolderIfNotExists.jsx
@@ -1,13 +1,14 @@
 #include "../../../utils/checkIfHasItBlendingColorFolder.jsx"
 #include "./createFolderIn.jsx"
 #include "./createRGBLayerIn.jsx"
+#include "../../../../config/readColorFolderName.jsx"
 
 function createCOLORFolderIfNotExists() {
     var doc = app.activeDocument;
 
     var hasItBlendingColorsFolder = checkIfHasItBlendingColorFolder();
     if (hasItBlendingColorsFolder) {
-        alert('There is already "COLORS" folder in top hierarchy');
+        alert('There is already "' + readColorFolderName() + '" folder in top hierarchy');
 
     } else {
         var COLORSFolder = doc.layerSets.add();

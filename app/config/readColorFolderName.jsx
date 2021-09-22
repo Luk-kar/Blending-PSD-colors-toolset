014bc837db543febc6f70bf3385f3c61a24ccda3
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-09-22 15:10:50 +0200

    folder name works

---
+++ app/config/readColorFolderName.jsx
@@ -0,0 +1,67 @@
+function readColorFolderName() {
+
+    var configFile = getConfigFile();
+
+    if (!configFile.exists) {
+        createConfigFile();
+    }
+
+    configFile.open("r");
+
+    var searchedPhrase = "color_folder_name = ";
+
+    return getValueFromKey(searchedPhrase, configFile);
+}
+
+alert(readColorFolderName());
+
+function getValueFromKey(key, FileIni) {
+
+    var lines = [];
+    var lineNumber = 0;
+
+    while (!FileIni.eof) {
+
+        lines[lineNumber] = FileIni.readln();
+
+        if (lines[lineNumber].search( key) != -1) {
+            var matichingLine = (lines[lineNumber]);
+
+            const regex = /[^= ]*$/g;
+            var foundValue = matichingLine.match(regex)
+            return foundValue;
+        }
+
+        lineNumber++;
+    }
+}
+
+function createConfigFile() {
+    var configDefaultText = "" +
+        "[FILE STRUCTURE]\n" +
+        "color_folder_name = COLORS\n" +
+        "base_folder_name = BASE\n" +
+        "layers = R, G, B\n" +
+        "[COLORS RGB]\n" +
+        "0 = 255, 0, 0\n" +
+        "1 = 0, 255, 0\n" +
+        "2 = 0, 0, 255";
+
+    configFile.open("w");
+    configFile.write(configDefaultText);
+    configFile.close();
+}
+
+function getConfigFile() {
+    var configFilePath = getConfigPath();
+
+    var configFile = new File(configFilePath);
+    return configFile;
+}
+
+function getConfigPath() {
+    var configFileName = "config.ini";
+
+    var congiFilePath = ((new File($.fileName)).parent.parent.parent).toString().replace(/\\/g, '/') + "/" + configFileName;
+    return congiFilePath;
+}

8782f2a3727d8a885b288aa846d44cdb624cd9c8
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-10-24 18:14:32 +0200

    update

--- app/eventHandlers/setColorFromCSVFile/helpers/getColorsErrors/helpers/getCSVAndPSDDiffrentColorsFolders.jsx
+++ app/eventHandlers/setColorFromCSVFile/helpers/getColorsErrors/helpers/getCSVAndPSDDiffrentColorsFolders.jsx
@@ -2,53 +2,23 @@
 
 function getCSVAndPSDDiffrentColorsFolders(CSVCOLORSFolders, foldersInCOLORS, CSV) { // todo
 
-    var foldersInCOLORSNames = [];
-    for (var i = 0; i < foldersInCOLORS.length; i++) {
-        foldersInCOLORSNames.push(foldersInCOLORS[i].name);
-    }
-    var CSVCOLORSFoldersNames = [];
-    for (var j = 1; j < CSVCOLORSFolders.length; j++) {
-        CSVCOLORSFoldersNames.push(CSVCOLORSFolders[j][0]);
-    }
+    var foldersInCOLORSNames = getFoldersInCOLORSNames(foldersInCOLORS);
+
+    var CSVCOLORSFoldersNames = getCSVCOLORSFoldersNames(CSVCOLORSFolders);
 
     var diffrentColorFolders = "";
     var diffrentColorFoldersColumns = "file,diffrent folders\n";
 
     var diffrentCSVTitle = CSV.fullName + ",";
-    var diffrentFoldersCSV = "";
 
-    for (var k = 0; k < CSVCOLORSFoldersNames.length; k++) {
-        var thereIs = false;
-        for (var l = 0; l < foldersInCOLORSNames.length; l++) {
-            if (CSVCOLORSFoldersNames[k] === foldersInCOLORSNames[l]) {
-                thereIs = true;
-                break;
-            }
-        }
-        if (!thereIs) {
-            diffrentFoldersCSV += CSVCOLORSFoldersNames[k] + " ";
-        }
-    }
+    var diffrentFoldersCSV = getDiffrentDiffrentFoldersCSV(CSVCOLORSFoldersNames, foldersInCOLORSNames)
 
     if (diffrentFoldersCSV) {
         diffrentColorFolders += diffrentCSVTitle + diffrentFoldersCSV;
     }
 
     var diffrentPSDTitle = "\n" + getFullActivePSDPath() + ",";
-    var diffrentFoldersPSD = "";
-
-    for (var k = 0; k < foldersInCOLORSNames.length; k++) {
-        var thereIs = false;
-        for (var l = 0; l < CSVCOLORSFoldersNames.length; l++) {
-            if (foldersInCOLORSNames[k] === CSVCOLORSFoldersNames[l]) {
-                thereIs = true;
-                break;
-            }
-        }
-        if (!thereIs) {
-            diffrentFoldersPSD += foldersInCOLORSNames[k] + " ";
-        }
-    }
+    var diffrentFoldersPSD = getDiffrentFoldersPSD(CSVCOLORSFoldersNames, foldersInCOLORSNames) 
 
     if (diffrentFoldersPSD) {
         diffrentColorFolders += diffrentPSDTitle + diffrentFoldersPSD;
@@ -64,4 +34,60 @@ function getCSVAndPSDDiffrentColorsFolders(CSVCOLORSFolders, foldersInCOLORS, CS
     }
 
     return diffrentColorFolders;
+}
+
+function getCSVCOLORSFoldersNames(CSVCOLORSFolders) {
+    var CSVCOLORSFoldersNames = [];
+    for (var j = 1; j < CSVCOLORSFolders.length; j++) {
+        CSVCOLORSFoldersNames.push(CSVCOLORSFolders[j][0]);
+    }
+    return CSVCOLORSFoldersNames;
+}
+
+function getFoldersInCOLORSNames(foldersInCOLORS) {
+    var foldersInCOLORSNames = [];
+    for (var i = 0; i < foldersInCOLORS.length; i++) {
+        foldersInCOLORSNames.push(foldersInCOLORS[i].name);
+    }
+    return foldersInCOLORSNames;
+}
+
+function getDiffrentDiffrentFoldersCSV(CSVCOLORSFoldersNames, foldersInCOLORSNames) {
+
+    var diffrentFoldersCSV = "";
+
+    for (var i = 0; i < CSVCOLORSFoldersNames.length; i++) {
+        var thereIs = false;
+        for (var l = 0; l < foldersInCOLORSNames.length; l++) {
+            if (CSVCOLORSFoldersNames[i] === foldersInCOLORSNames[l]) {
+                thereIs = true;
+                break;
+            }
+        }
+        if (!thereIs) {
+            diffrentFoldersCSV += CSVCOLORSFoldersNames[i] + " ";
+        }
+    }
+
+    return diffrentFoldersCSV
+}
+
+function getDiffrentFoldersPSD(CSVCOLORSFoldersNames, foldersInCOLORSNames) {
+
+    var diffrentFoldersPSD = "";
+
+    for (var k = 0; k < foldersInCOLORSNames.length; k++) {
+        var thereIs = false;
+        for (var l = 0; l < CSVCOLORSFoldersNames.length; l++) {
+            if (foldersInCOLORSNames[k] === CSVCOLORSFoldersNames[l]) {
+                thereIs = true;
+                break;
+            }
+        }
+        if (!thereIs) {
+            diffrentFoldersPSD += foldersInCOLORSNames[k] + " ";
+        }
+    }
+
+    return diffrentFoldersPSD
 }

f2f0cd246233cf5c357dcd1aa11fa5a433bff42d
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-10-24 16:45:13 +0200

    update

--- app/config/read/utils/createConfigFile.jsx
+++ app/config/read/utils/createConfigFile.jsx
@@ -4,22 +4,28 @@ function createConfigFile() {
 
     var configFile = getConfigFile();
 
+    var fileStructure = configDefaultValues.fileStructure
     var color_folder = configDefaultValues.fileStructure.color_folder
     var base_folder = configDefaultValues.fileStructure.base_folder;
     var colorsRGB = configDefaultValues.colorsRGB;
+    var pickerCoords = configDefaultValues.pickerCoords
 
     var _ = " = ";
 
     var configDefaultText = "" +
-        "[" + configDefaultValues.fileStructure.title + "]\n" +
+        "[" + fileStructure.title + "]\n" +
         color_folder.name.key + _ + color_folder.name.value + "\n" +
         color_folder.folders.key + _ + color_folder.folders.value + "\n" +
         color_folder.layers.key + _ + color_folder.layers.value + "\n" +
         base_folder.name.key + _ + base_folder.name.value + "\n" +
-        "[" + configDefaultValues.colorsRGB.title + "]\n" +
-        colorsRGB.R.key + _ + colorsRGB.R.value + "\n" + // todo create number of folder in object
+        "[" + colorsRGB.title + "]\n" +
+        colorsRGB.R.key + _ + colorsRGB.R.value + "\n" +
         colorsRGB.G.key + _ + colorsRGB.G.value + "\n" +
-        colorsRGB.B.key + _ + colorsRGB.B.value + "\n";
+        colorsRGB.B.key + _ + colorsRGB.B.value + "\n" +
+        "[" + pickerCoords.title + "]\n" +
+        pickerCoords.R.key + _ + pickerCoords.R.value + "\n" +
+        pickerCoords.G.key + _ + pickerCoords.G.value + "\n" +
+        pickerCoords.B.key + _ + pickerCoords.B.value + "\n";
 
     configFile.open("w");
     configFile.write(configDefaultText);

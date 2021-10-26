8752f2c155b20adacbdf96e8a3e7aa4851a6195c
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-10-26 12:57:56 +0200

    update

--- app/eventHandlers/writeColorsFromLayersToCSV/helpers/changeLayersFolderAttributes/prearrangeDocToProcess.jsx
+++ app/eventHandlers/writeColorsFromLayersToCSV/helpers/changeLayersFolderAttributes/prearrangeDocToProcess.jsx
@@ -13,4 +13,31 @@ function prearrangeDocToProcess(COLORSFolder, RGBfolders, layersNames) {
 
     doBlendingModesNormal(RGBfolders);
     doBlendingModesNormalAllLayersInAllFolders(RGBfolders, layersNames);
+
+    sortRGBLayersInOrderInFolders(RGBfolders, layersNames)
+}
+
+function sortRGBLayersInOrderInFolders(RGBfolders, layersNames) {
+
+    for (var i = 0; i < RGBfolders.length; i++) {
+
+        var folderRGB = RGBfolders[i]
+        var layersRGB = folderRGB.artLayers
+
+        sortRGBLayersInOrder(layersNames, layersRGB)
+    }
+}
+
+function sortRGBLayersInOrder(layersNames, layersRGB) {
+
+    var counter = 0
+
+    for (var j = 0; j < layersNames.length; j++) {
+        var RGBLayer = layersRGB.getByName(layersNames[j])
+        moveBeforeLayer(RGBLayer, layersRGB[j])
+    }
+}
+
+function moveBeforeLayer(LayerToMove,LayerBefore) {
+    LayerToMove.move(LayerBefore, ElementPlacement.PLACEBEFORE)
 }
